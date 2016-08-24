748f002a6892bfd3d07086e25982e459ed746353
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2016-08-24 14:22:48 -0700

    Jasmine test: replaced let and const with var for old node compatibility

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -2,18 +2,18 @@
 
 "use strict";
 
-let wsdotRouteUtils = require("../index");
-let RouteDescription = wsdotRouteUtils.default;
-let getRouteParts = wsdotRouteUtils.getRouteParts;
+var wsdotRouteUtils = require("../index");
+var RouteDescription = wsdotRouteUtils.default;
+var getRouteParts = wsdotRouteUtils.getRouteParts;
 
 describe("RouteDescription", () => {
     it("won't parse invalidly formatted routes", () => {
-        let srid = "005AAA";
-        expect(() => { let thing = new RouteDescription(srid); }).toThrowError();
+        var srid = "005AAA";
+        expect(() => { var thing = new RouteDescription(srid); }).toThrowError();
     });
     it("can parse mainline routes", () => {
-        const srid = "005";
-        let desc = new RouteDescription(srid);
+        var srid = "005";
+        var desc = new RouteDescription(srid);
         expect(desc.sr).toEqual(srid);
         expect(desc.rrt).toBeNull();
         expect(desc.rrq).toBeNull();
@@ -23,34 +23,34 @@ describe("RouteDescription", () => {
         expect(desc.toString()).toEqual(srid);
     });
     it("can parse route with RRT && RRQ", () => {
-        const srid = "101COABERDN";
-        let [sr, rrt, rrq] = getRouteParts(srid);
-        let desc = new RouteDescription(srid);
+        var srid = "101COABERDN";
+        var [sr, rrt, rrq] = getRouteParts(srid);
+        var desc = new RouteDescription(srid);
         expect(desc.sr).toEqual(sr);
         expect(desc.rrt).toEqual(rrt);
         expect(desc.rrq).toEqual(rrq);
-        expect(desc.rrtDescription).toEqual("Couplet");
+        expect(desc.rrtDescription).toEqual("Coupvar");
         expect(desc.rrqDescription).toEqual("Aberdeen");
         expect(desc.mainlineConnectionMP).toBeNull();
     });
     it("can parse MP from ramp ID", () => {
-        const srid = "005R109958";
-        let desc = new RouteDescription(srid);
+        var srid = "005R109958";
+        var desc = new RouteDescription(srid);
         expect(desc.mainlineConnectionMP).toEqual(99.58);
     });
 
     it('can optionally support direction', function() {
-        const withDirId = "529SPEVERETd";
-        const nonDirId = "005Q516479";
+        var withDirId = "529SPEVERETd";
+        var nonDirId = "005Q516479";
 
-        let withDirDesc = new RouteDescription(withDirId, true);
-        let nonDirDesc = new RouteDescription(nonDirId, false);
-        let incDesc = new RouteDescription(nonDirId, true);
+        var withDirDesc = new RouteDescription(withDirId, true);
+        var nonDirDesc = new RouteDescription(nonDirId, false);
+        var incDesc = new RouteDescription(nonDirId, true);
         expect(withDirDesc.isDecrease).toEqual(true);
         expect(nonDirDesc.isDecrease).toEqual(null);
         expect(incDesc.isDecrease).toEqual(false);
         expect(() => {
-            let desc = new RouteDescription(withDirId, false);
+            var desc = new RouteDescription(withDirId, false);
         }).toThrowError();
     });
 
